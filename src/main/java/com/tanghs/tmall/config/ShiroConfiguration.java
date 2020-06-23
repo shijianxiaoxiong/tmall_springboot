19151e0a93c5bd2184a9f141408674ab81c18f0e
shijianxiaoxiong <38215175+shijianxiaoxiong@users.noreply.github.com>
2020-06-23 13:55:58 +0800

    添加shiro继承JPARealm的注释

--- src/main/java/com/tanghs/tmall/config/ShiroConfiguration.java
+++ src/main/java/com/tanghs/tmall/config/ShiroConfiguration.java
@@ -8,9 +8,12 @@ import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
- 
+
 import com.tanghs.tmall.realm.JPARealm;
  //shiro相关配置
+//进行 Shiro配置，其中的 bean 都是常规配置，需要关注的有两个：
+//1. getJPARealm() 指定了 Realm 使用 JPARealm。
+//2. hashedCredentialsMatcher() 指定了 加密算法使用 md5,并且混进行2次加密。
 @Configuration
 public class ShiroConfiguration {
     @Bean
